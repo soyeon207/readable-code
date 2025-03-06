012d45c725fecd81bc118c86345c827856b9fef4
soyeon207 <psy010207@naver.com>
2025-03-06 14:12:00 +0900

    🎨[3-6] LSP

--- src/main/java/cleancode/minesweeper/tobe/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/GameBoard.java
@@ -1,5 +1,9 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.cell.Cell;
+import cleancode.minesweeper.tobe.cell.EmptyCell;
+import cleancode.minesweeper.tobe.cell.LandMineCell;
+import cleancode.minesweeper.tobe.cell.NumberCell;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 
 import java.util.Arrays;
@@ -22,15 +26,14 @@ public class GameBoard {
 
         for (int row = 0; row < rowSize; row++) {
             for (int col = 0; col < colSize; col++) {
-                board[row][col] = Cell.create();
+                board[row][col] = new EmptyCell();
             }
         }
 
         for (int i = 0; i < landMineCount; i++) {
             int landMineCol = new Random().nextInt(colSize);
             int landMineRow = new Random().nextInt(rowSize);
-            Cell landMineCell = findCell(landMineRow, landMineCol);
-            landMineCell.turnOnLandMine();
+            board[landMineRow][landMineCol] = new LandMineCell();
         }
 
         for (int row = 0; row < rowSize; row++) {
@@ -39,8 +42,11 @@ public class GameBoard {
                     continue;
                 }
                 int count = countNearByLandMines(row, col);
-                Cell cell = findCell(row, col);
-                cell.updateNearByLandMineCount(count);
+                if (count == 0) {
+                    continue;
+                }
+                NumberCell numberCell = new NumberCell(count);
+                board[row][col] = numberCell;
             }
         }
     }
---
+++ src/main/java/cleancode/minesweeper/tobe/cell/NumberCell.java
@@ -0,0 +1,31 @@
+package cleancode.minesweeper.tobe.cell;
+
+public class NumberCell extends Cell {
+    private final int nearbyLandMineCount;
+
+    public NumberCell(int count) {
+        this.nearbyLandMineCount = count;
+    }
+
+    @Override
+    public boolean isLandMine() {
+        return false;
+    }
+
+    @Override
+    public boolean hasLandMineCount() {
+        return true;
+    }
+
+    @Override
+    public String getSign() {
+        if (isOpened) {
+            return String.valueOf(nearbyLandMineCount);
+        }
+
+        if (isFlagged) {
+            return FLAG_SIGN;
+        }
+        return UNCHECKED_SIGN;
+    }
+}
